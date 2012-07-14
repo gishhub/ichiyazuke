c00b22d77b1615b11720dc903e381932a26f1e74
naratakahito <gooddaytnn@gmail.com>
2012-07-14 20:36:35 +0900

    テスト

--- ichiyazuke_web/src/init/Initialize.java
+++ ichiyazuke_web/src/init/Initialize.java
@@ -13,7 +13,7 @@ public class Initialize extends HttpServlet {
 
 	public void init(){
 //		ConfReader.getInstance().readConf("/Users/NaraTakahito/Documents/workspace_servlet/ichiyazuke_web/ichiyazuke_web/src/init/ichiyazuke.properties");
-		ConfReader.getInstance().readConf("/home/gishhub/onenight/etc/ichiyazuke.properties");
+//		ConfReader.getInstance().readConf("/home/gishhub/onenight/etc/ichiyazuke.properties");
 
 	}
 }
